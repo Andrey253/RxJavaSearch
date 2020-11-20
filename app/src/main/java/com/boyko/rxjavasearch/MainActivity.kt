7b06679a658b48fcc186a33ada9d1a79490c8201
Andrey <Andrey253@yandex.ru>
2020-11-20 20:17:09 +0700

    Нужна помощь

--- app/src/main/java/com/boyko/rxjavasearch/MainActivity.kt
+++ app/src/main/java/com/boyko/rxjavasearch/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.boyko.rxjavasearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Observable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.boyko.rxjavasearch.Text.Companion.TEXT
+import com.google.android.material.textfield.TextInputEditText
+import io.reactivex.Flowable
+import java.util.*
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtv:TextView
+    lateinit var searchText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.textView4).text = TEXT
+
+
+        edtv = findViewById<TextView>(R.id.textView4)
+        edtv.text = TEXT
+        searchText = findViewById<TextInputEditText>(R.id.searchtext)
+//        searchText.addTextChangedListener() { it ->
+//        Подписаться на изменение текста получилось, но не могу сделать Flowable.debounce для этого едит текста
+//        ПРОШУ ПОМОЩИ
+//        }
+        val flowable = Flowable.just(searchText)
+        flowable.debounce(1L, TimeUnit.SECONDS).subscribe {
+            println("mytag $it")
+            // println("mytag ${searchText.text}")
+        }
     }
 }
